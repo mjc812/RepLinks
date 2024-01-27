e1b055b53211ac8436356b754747f7d9dbe8d886
mjc812 <68936604+mjc812@users.noreply.github.com>
2024-01-26 20:22:44 -0800

    Add fucntion to find caption link in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -6,6 +6,21 @@ const handleMutation = (mutationsList, mutationObserver) => {
     }
 };
 
+const getCaptionLink = () => {
+    const postContents = document.querySelectorAll('div[data-test-id="post-content"]');
+
+    if (postContents.length > 0) {
+        const postContainer = postContents[0];
+        const imageSection = postContainer.querySelector(':nth-child(5)');
+
+        const captionLink = imageSection.querySelector('span:has(> a)');
+        
+        if (captionLink) return captionLink;
+    }
+    
+    return null;
+};
+
 const targetObservationNode = document.body;
 const observationConfig = { childList: true, subtree: true };
 const mutationObserver = new MutationObserver(handleMutation);
